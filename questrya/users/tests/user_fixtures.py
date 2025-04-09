73e223fb35bfe076cf44747bc23250711c76a6ab
tiago.paranhos <tiagoprn@gmail.com>
2025-04-09 07:12:34 -0300

    test: updated fixture

--- questrya/users/tests/user_fixtures.py
+++ questrya/users/tests/user_fixtures.py
@@ -6,11 +6,13 @@ from typing import Dict
 
 import pytest
 
+from questrya.common.value_objects.email import Email
+
 
 @pytest.fixture
 def domain_user_data_picard() -> Dict:
     return {
         'username': 'picard',
-        'email': 'jean_luc_picard@enterprise.org',
+        'email': Email('jean_luc_picard@enterprise.org'),
         'password': '12345678',
     }
