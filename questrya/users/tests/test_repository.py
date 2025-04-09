fabb49940b421ebbe4894f4f0704f5d27a7329bb
tiago.paranhos <tiagoprn@gmail.com>
2025-04-09 08:12:36 -0300

    tests: user repository get methods

--- questrya/users/tests/test_repository.py
+++ questrya/users/tests/test_repository.py
@@ -43,3 +43,41 @@ class TestUserRepository:
         updated_domain_user = UserRepository.get_by_email(email=domain_user.email)
         assert updated_domain_user.email.address == new_email.address
         assert updated_domain_user.email.address != original_email.address
+
+    def test_get_by_uuid(self, domain_user_data_picard, db_session):
+        # GIVEN
+        domain_user = User(**domain_user_data_picard)
+        domain_user = UserRepository.save(user=domain_user)  # creates the user
+        assert domain_user.uuid
+
+        # WHEN
+        found_domain_user = UserRepository.get_by_uuid(uuid=domain_user.uuid)
+
+        # THEN
+        assert found_domain_user
+        assert isinstance(found_domain_user, User)
+        for key, value in domain_user_data_picard.items():
+            if key == 'password':
+                password_hash = getattr(found_domain_user, 'password_hash')
+                assert isinstance(password_hash, str)
+                continue
+            assert getattr(found_domain_user, key) == value
+
+    def test_get_by_username(self, domain_user_data_picard, db_session):
+        # GIVEN
+        domain_user = User(**domain_user_data_picard)
+        domain_user = UserRepository.save(user=domain_user)  # creates the user
+        assert domain_user.uuid
+
+        # WHEN
+        found_domain_user = UserRepository.get_by_username(username='picard')
+
+        # THEN
+        assert isinstance(found_domain_user, User)
+        assert isinstance(found_domain_user.uuid, UUID)
+        for key, value in domain_user_data_picard.items():
+            if key == 'password':
+                password_hash = getattr(found_domain_user, 'password_hash')
+                assert isinstance(password_hash, str)
+                continue
+            assert getattr(found_domain_user, key) == value
